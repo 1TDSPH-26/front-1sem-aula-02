188eee6b361dd17280f19a23a415d6e41d3f573a
Euthiaguera <piedadethiago7@gmail.com>
2026-05-08 09:50:49 -0300

    feat: Introdução ao javascricpt com variáveis.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// declaração de variáveis com JS
+// 3 Tipos = var / let/ const
+/*
+*Comentário de Blocao
+*/
+
+//Declaração com var:
+// var nome = "Fiap";
+// console.log("A melhor faculdade :"  + nome);
+
+// var nr1 = 10;
+// var nr2 = "5";
+// console.log("Resultado da soma : " + (nr1 + parseInt(nr2)));
+
+var nome = "Thiago";
+console.log(typeof variavel);
+
+variavel = 100;
+console.log(typeof variavel)
+
+variavel = true;
+console.log(typeof variavel)
+
+variavel = null;
+console.log(typeof variavel)
+
+variavel = 'a';
+console.log(typeof variavel)
+
+// criando um ternário e mexendo com cálculos
+let idade = 17;
